6103de98dce7ce928f5f70bc273b935009aa7579
Dimitar Chukovski <59423614+chukovskid@users.noreply.github.com>
2020-02-22 11:00:49 +0100

    Added EntityFramework, Migration with Console comands

---
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Product> Products { get; set;}
+        public DbSet<ProductCategory> ProductsCategories { get; set;}
+
+
+    }
+}
