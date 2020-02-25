c035dad1cac4e83144a54a052408c6e281037344
Dimitar Chukovski <59423614+chukovskid@users.noreply.github.com>
2020-02-25 19:05:49 +0100

    Added Basket, Basket and BasketItem Models. Added migration and Updated for SQL

--- MyShop/MyShop.DataAccess.SQL/DataContext.cs
+++ MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -17,6 +17,8 @@ namespace MyShop.DataAccess.SQL
 
         public DbSet<Product> Products { get; set;}
         public DbSet<ProductCategory> ProductsCategories { get; set;}
+        public DbSet<Basket> Baskets { get; set;}
+        public DbSet<BasketItem> BasketItems { get; set;}
 
 
     }
